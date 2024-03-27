d605090def3af09b09f75e501049a20b8d4c402c
Andre Cortes <52327276+andrecortes1997@users.noreply.github.com>
2024-03-27 11:00:44 +0000

    1st Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const getRandomElement = (array) => {
+  const randomIndex = Math.floor(Math.random()* array.length)
+    return array[randomIndex]
+    
+  }
+  
+  const getGuilty = () => {
+    const who =  ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    return getRandomElement(who);
+  } 
+  
+  const getAction = () => {
+    const action =  ['ate', 'peed', 'crushed', 'broke'];
+    return getRandomElement(action);
+  } 
+  
+  const getTarget = () => {
+    const target =  ['my homework', 'my phone', 'the car'];
+    return getRandomElement(target);
+  } 
+  
+  const getWhen = () => {
+    const when =  ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    return getRandomElement(when);
+  } 
+  
+  const getMessage = (guilty, action, target, when) =>{
+  return `${guilty} ${action} ${target} ${when}`; 
+  }
+  
+  const printMessage = (message) => {
+    console.log(message)
+  }
+  
+  function main(){
+  const guilty = getGuilty();
+  const action = getAction();
+  const target = getTarget();
+  const when = getWhen();
+  
+  const message = getMessage(guilty, action, target, when);
+  printMessage(message);
+  }
+  
+  main();
